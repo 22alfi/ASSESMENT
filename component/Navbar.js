df61fe0487339e061791cdb201ebb552b262e35a
22alfi <alfifaisy@gmail.com>
2023-05-12 21:50:46 +0530

    firstcommit

---
+++ component/Navbar.js
@@ -0,0 +1,93 @@
+import {  Box, Button, Checkbox , Container, CssBaseline, FormControlLabel, Grid, Link, TextField, Typography} from '@mui/material'
+
+
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+       
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form"  noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address / Username"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+             <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="age"
+              label="age"
+              type="age"
+              id="age"
+              autoComplete="age"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color='error'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        
+       
+      </Container>
+    
+    </div>
+  )
+}
+
+
+
+export default Navbar
